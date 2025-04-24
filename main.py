168c62b3ad2b7980e1a7ffd25690a34726255694
Sebastien Perre <58455280+sebperre@users.noreply.github.com>
2025-04-24 11:51:01 -0700

    add versioning and fix gesture detectiona

--- main.py
+++ main.py
@@ -34,28 +34,37 @@ options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path='gesture_recognizer.task'),
     running_mode=VisionRunningMode.LIVE_STREAM,
     result_callback=save_gesture)
+
 recognizer = GestureRecognizer.create_from_options(options)
-prev_timestamp_ms = 0
 
 def detect_gesture():
     cap = cv2.VideoCapture(0)
-
+    prev_timestamp_ms = 0
     while cap.isOpened():
         ret, frame = cap.read()
+
         if not ret:
             print("Failed to capture a frame")
             break
+
+
         current_timestamp_ms = int(time.time() * 1000)
-        if current_timestamp_ms > prev_timestamp_ms:
+        if current_timestamp_ms > prev_timestamp_ms+2000:
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
             recognizer.recognize_async(mp_image, current_timestamp_ms)
+            prev_timestamp_ms = current_timestamp_ms
+
+        cv2.putText(frame, str(last_gesture), (250, 50), 
+            cv2.FONT_HERSHEY_COMPLEX, 0.9, 
+            (0, 255, 0), 2)
+        
         cv2.imshow('Camera Feed', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+
     # Release the camera and close all windows
     cap.release()
     cv2.destroyAllWindows()
-
 
 detect_gesture()
